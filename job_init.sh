428cbdd8893b6b1bbfe6c16db1b2dee1488d4663
Anuj Patel <91538343+anuj-p@users.noreply.github.com>
2023-06-22 15:30:30 -0500

    Update job_init.sh

--- job_init.sh
+++ job_init.sh
@@ -2,6 +2,9 @@
 
 # GET JOB FILES
 git clone $JOB_FILES_URL /compute_shared/job_files
+chmod +x /compute_shared/job_files/preprocess.sh
+chmod +x /compute_shared/job_files/job.sh
+chmod +x /compute_shared/job_files/postprocess.sh
 # PRE-PROCESSING
 cd /compute_shared/job_files
 bash preprocess.sh
