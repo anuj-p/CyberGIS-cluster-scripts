9ca3aea00f6c388a1e035870b9088a670420d4b0
Anuj Patel <91538343+anuj-p@users.noreply.github.com>
2023-06-22 15:21:15 -0500

    Update job_queue.sh

--- job_queue.sh
+++ job_queue.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
+# ADD JOB TO QUEUE
 sbatch --ntasks=$NTASKS --cpus-per-task=$CPUS_PER_TASK --output=/compute_shared/output/job-%j.out --wait /home/ubuntu/job_exec.sh
 bash /home/ubuntu/job_cleanup.sh
