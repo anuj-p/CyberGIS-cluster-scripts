9cce4238cf97d6c7d2e05ea8591f3be50b95a7ac
Anuj Patel <91538343+anuj-p@users.noreply.github.com>
2023-06-22 15:03:18 -0500

    Update job_queue.sh

--- job_queue.sh
+++ job_queue.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-sbatch --ntasks=$NTASKS --cpus-per-task=$CPUS_PER_TASK --output=/compute_shared/output/job-%j.out /compute_shared/job_files/job_exec.sh
+sbatch --ntasks=$NTASKS --cpus-per-task=$CPUS_PER_TASK --output=/compute_shared/output/job-%j.out /home/ubuntu/job_exec.sh
