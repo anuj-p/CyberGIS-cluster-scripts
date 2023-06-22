e0961e4088e917e392cf860588d49bec08e89978
Anuj Patel <91538343+anuj-p@users.noreply.github.com>
2023-06-22 14:11:29 -0500

    Update head_init.sh

--- head_init.sh
+++ head_init.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 
 apt install git -y
-cd /home/ubuntu
-curl -o job_init.sh https://raw.githubusercontent.com/anuj-p/CyberGIS-cluster-scripts/main/job_init.sh
+curl -o /home/ubuntu/job_init.sh https://raw.githubusercontent.com/anuj-p/CyberGIS-cluster-scripts/main/job_init.sh
