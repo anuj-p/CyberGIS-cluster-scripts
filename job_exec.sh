abf315cdbae4a007bbf987e0332070655d577051
Anuj Patel <91538343+anuj-p@users.noreply.github.com>
2023-06-22 15:02:28 -0500

    Update job_exec.sh

--- job_exec.sh
+++ job_exec.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-sbatch --ntasks=$NTASKS --cpus-per-task=$CPUS_PER_TASK --output=/compute_shared/output/job-%j.out /compute_shared/job_files/job.sh
+cd /compute_shared/job_files
+bash job.sh
