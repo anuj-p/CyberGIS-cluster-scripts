d3bb64391f681019ab3b070e7c87aead3db49e52
Anuj Patel <91538343+anuj-p@users.noreply.github.com>
2023-06-27 10:40:02 -0500

    Revert head_init.sh

--- head_init.sh
+++ head_init.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 # PREPARE HEAD NODE
-# apt install git -y
-# curl -o /home/ubuntu/job_init.sh https://raw.githubusercontent.com/anuj-p/CyberGIS-cluster-scripts/main/job_init.sh
+apt install git -y
+curl -o /home/ubuntu/job_init.sh https://raw.githubusercontent.com/anuj-p/CyberGIS-cluster-scripts/main/job_init.sh
 curl -o /home/ubuntu/job_queue.sh https://raw.githubusercontent.com/anuj-p/CyberGIS-cluster-scripts/main/job_queue.sh
 curl -o /home/ubuntu/job_exec.sh https://raw.githubusercontent.com/anuj-p/CyberGIS-cluster-scripts/main/job_exec.sh
 curl -o /home/ubuntu/job_cleanup.sh https://raw.githubusercontent.com/anuj-p/CyberGIS-cluster-scripts/main/job_cleanup.sh
-# chmod +x /home/ubuntu/job_init.sh
+chmod +x /home/ubuntu/job_init.sh
 chmod +x /home/ubuntu/job_queue.sh
 chmod +x /home/ubuntu/job_exec.sh
 chmod +x /home/ubuntu/job_cleanup.sh
