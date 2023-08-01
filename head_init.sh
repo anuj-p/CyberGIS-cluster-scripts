8e2ab44a3d20a9e227bc7db26b8878d25701606e
Anuj Patel <91538343+anuj-p@users.noreply.github.com>
2023-07-31 22:45:29 -0500

    Update head_init.sh

--- head_init.sh
+++ head_init.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
 # PREPARE HEAD NODE
+curl -o /home/ubuntu/start_notebook.sh https://raw.githubusercontent.com/anuj-p/CyberGIS-cluster-scripts/main/start_notebook.sh
+chmod +x /home/ubuntu/start_notebook.sh
+
 apt install python3-mpi4py -y
 
 pip3 install notebook
