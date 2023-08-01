fca872026c2afdae41c6d790f50297b8338a6e97
Anuj Patel <91538343+anuj-p@users.noreply.github.com>
2023-07-31 20:57:51 -0500

    Update head_init.sh

--- head_init.sh
+++ head_init.sh
@@ -1,15 +1,7 @@
 #!/bin/bash
 
 # PREPARE HEAD NODE
-apt install git -y
-curl -o /home/ubuntu/job_init.sh https://raw.githubusercontent.com/anuj-p/CyberGIS-cluster-scripts/main/job_init.sh
-curl -o /home/ubuntu/job_queue.sh https://raw.githubusercontent.com/anuj-p/CyberGIS-cluster-scripts/main/job_queue.sh
-curl -o /home/ubuntu/job_exec.sh https://raw.githubusercontent.com/anuj-p/CyberGIS-cluster-scripts/main/job_exec.sh
-curl -o /home/ubuntu/job_cleanup.sh https://raw.githubusercontent.com/anuj-p/CyberGIS-cluster-scripts/main/job_cleanup.sh
-chmod +x /home/ubuntu/job_init.sh
-chmod +x /home/ubuntu/job_queue.sh
-chmod +x /home/ubuntu/job_exec.sh
-chmod +x /home/ubuntu/job_cleanup.sh
+apt install python3-mpi4py -y
 
 pip3 install notebook
 pip3 install ipyparallel
