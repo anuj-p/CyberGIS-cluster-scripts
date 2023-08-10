cf5a739c37c06eefb71266f9cede7edd05d07cf9
Anuj Patel <91538343+anuj-p@users.noreply.github.com>
2023-08-10 15:51:32 -0500

    Update start_notebook.sh

--- start_notebook.sh
+++ start_notebook.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 dns_name=$(curl -s http://169.254.169.254/latest/meta-data/public-hostname)
-nohup python3 -m notebook --no-browser --port=8888 --ip=${dns_name} --NotebookApp.token='' --NotebookApp.password='' --allow-root &!
+python3 -m notebook --no-browser --port=8888 --ip=${dns_name} --NotebookApp.token='' --NotebookApp.password='' --allow-root
