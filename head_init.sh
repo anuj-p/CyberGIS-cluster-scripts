0b65d9bf0169b37968be028be8c90240209cd077
Anuj Patel <91538343+anuj-p@users.noreply.github.com>
2023-08-10 15:52:49 -0500

    Update head_init.sh

--- head_init.sh
+++ head_init.sh
@@ -10,4 +10,6 @@ apt install python3-mpi4py -y
 pip3 install notebook
 pip3 install ipyparallel
 
-bash /home/ubuntu/start_notebook.sh
+# bash /home/ubuntu/start_notebook.sh
+dns_name=$(curl -s http://169.254.169.254/latest/meta-data/public-hostname)
+python3 -m notebook --no-browser --port=8888 --ip=${dns_name} --NotebookApp.token='' --NotebookApp.password='' --allow-root
