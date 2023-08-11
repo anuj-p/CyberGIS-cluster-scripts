8a501fedbce1a039a5d676e575ff96ce57c9826e
Anuj Patel <91538343+anuj-p@users.noreply.github.com>
2023-08-10 20:45:44 -0500

    Update head_init.sh

--- head_init.sh
+++ head_init.sh
@@ -8,6 +8,6 @@ apt install python3-mpi4py -y
 pip3 install notebook
 pip3 install ipyparallel
 
-cd /home/ubuntu
-dns_name=$(curl -s http://169.254.169.254/latest/meta-data/public-hostname)
-setsid nohup python3 -m notebook --no-browser --port=8888 --ip=${dns_name} --NotebookApp.token='' --NotebookApp.password='' --allow-root > /dev/null 2>&1 & disown
+# cd /home/ubuntu
+# dns_name=$(curl -s http://169.254.169.254/latest/meta-data/public-hostname)
+# setsid nohup python3 -m notebook --no-browser --port=8888 --ip=${dns_name} --NotebookApp.token='' --NotebookApp.password='' --allow-root > /dev/null 2>&1 & disown
