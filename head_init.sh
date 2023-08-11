6b6f6b06871364f532c140c582e00534ffc56c87
Anuj Patel <91538343+anuj-p@users.noreply.github.com>
2023-08-10 20:46:56 -0500

    Update head_init.sh

--- head_init.sh
+++ head_init.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 # PREPARE HEAD NODE
+curl -o /home/ubuntu/start_notebook.sh https://raw.githubusercontent.com/anuj-p/CyberGIS-cluster-scripts/main/start_notebook.sh
+chmod +x /home/ubuntu/start_notebook.sh
 curl -o /home/ubuntu/default.ipynb https://raw.githubusercontent.com/anuj-p/CyberGIS-cluster-scripts/main/default.ipynb
 
 apt install python3-mpi4py -y
