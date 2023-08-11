f25cba00832fbddd75d8e89cffa26699f3a6f545
Anuj Patel <91538343+anuj-p@users.noreply.github.com>
2023-08-10 21:47:43 -0500

    Update head_init.sh

--- head_init.sh
+++ head_init.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 
 # PREPARE HEAD NODE
-curl -o /home/ubuntu/start_notebook.sh https://raw.githubusercontent.com/anuj-p/CyberGIS-cluster-scripts/main/start_notebook.sh
-chmod +x /home/ubuntu/start_notebook.sh
 curl -o /home/ubuntu/default.ipynb https://raw.githubusercontent.com/anuj-p/CyberGIS-cluster-scripts/main/default.ipynb
 
 apt install python3-mpi4py -y
@@ -10,6 +8,7 @@ apt install python3-mpi4py -y
 pip3 install notebook
 pip3 install ipyparallel
 
-# cd /home/ubuntu
-# dns_name=$(curl -s http://169.254.169.254/latest/meta-data/public-hostname)
-# setsid nohup python3 -m notebook --no-browser --port=8888 --ip=${dns_name} --NotebookApp.token='' --NotebookApp.password='' --allow-root > /dev/null 2>&1 & disown
+cd /home/ubuntu
+su ubuntu
+dns_name=$(curl -s http://169.254.169.254/latest/meta-data/public-hostname)
+setsid nohup python3 -m notebook --no-browser --port=8888 --ip=${dns_name} --NotebookApp.token='' --NotebookApp.password='' --allow-root > /dev/null 2>&1 & disown
