210e481e7979d7acf4d4ea0fa8495370ce07c3df
Anuj Patel <91538343+anuj-p@users.noreply.github.com>
2023-08-12 21:41:23 -0500

    Update head_init.sh

--- head_init.sh
+++ head_init.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
 # PREPARE HEAD NODE
+apt install subversion -y
+sudo -Hiu ubuntu svn export https://github.com/anuj-p/CyberGIS-cluster-scripts/trunk/profile_slurm /home/ubuntu/.ipython
+
 curl -o /home/ubuntu/default.ipynb https://raw.githubusercontent.com/anuj-p/CyberGIS-cluster-scripts/main/default.ipynb
 
 apt install python3-mpi4py -y
@@ -9,5 +12,4 @@ pip3 install markupsafe==2.0.1 --force-reinstall
 pip3 install notebook
 pip3 install ipyparallel
 
-cd /home/ubuntu
 sudo -Hiu ubuntu setsid nohup python3 -m notebook --no-browser --port=8888 --ip=$(curl -s http://169.254.169.254/latest/meta-data/public-hostname) --NotebookApp.token='' --NotebookApp.password='' --allow-root > /dev/null 2>&1 & disown
