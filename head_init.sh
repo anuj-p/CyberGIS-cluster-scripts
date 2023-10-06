bb97cca5e822871aefb7ce3e5bbd22a54c2a3b7f
Anuj Patel <91538343+anuj-p@users.noreply.github.com>
2023-10-06 18:53:28 -0500

    Update head_init.sh

--- head_init.sh
+++ head_init.sh
@@ -12,4 +12,6 @@ pip3 install markupsafe==2.0.1 --force-reinstall
 pip3 install notebook
 pip3 install ipyparallel
 
+pip3 install numpy
+
 sudo -Hiu ubuntu setsid nohup python3 -m notebook --no-browser --port=8888 --ip=$(curl -s http://169.254.169.254/latest/meta-data/public-hostname) --NotebookApp.token='' --NotebookApp.password='' --allow-root > /dev/null 2>&1 & disown
