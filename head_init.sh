8feb39bd4116822ba710247fdc02575f515b4484
Anuj Patel <91538343+anuj-p@users.noreply.github.com>
2023-10-06 09:42:30 -0500

    Update head_init.sh

--- head_init.sh
+++ head_init.sh
@@ -12,4 +12,13 @@ pip3 install markupsafe==2.0.1 --force-reinstall
 pip3 install notebook
 pip3 install ipyparallel
 
+pip3 install numpy
+pip3 install pandas
+pip3 install fiona
+pip3 install pyproj
+pip3 install shapely
+pip3 install rtree
+pip3 install geopandas
+pip3 install access
+
 sudo -Hiu ubuntu setsid nohup python3 -m notebook --no-browser --port=8888 --ip=$(curl -s http://169.254.169.254/latest/meta-data/public-hostname) --NotebookApp.token='' --NotebookApp.password='' --allow-root > /dev/null 2>&1 & disown
