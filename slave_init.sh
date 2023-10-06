247e51cb91d04c0e51c1c12f92e3fceca16cbac4
Anuj Patel <91538343+anuj-p@users.noreply.github.com>
2023-10-06 18:53:15 -0500

    Update slave_init.sh

--- slave_init.sh
+++ slave_init.sh
@@ -6,10 +6,3 @@ apt install python3-mpi4py -y
 pip3 install ipyparallel
 
 pip3 install numpy
-pip3 install pandas
-pip3 install fiona
-pip3 install pyproj
-pip3 install shapely
-pip3 install rtree
-pip3 install geopandas
-pip3 install access
