aeb2f786a66199dc7ee3428549e877118c0cb3f5
Anuj Patel <91538343+anuj-p@users.noreply.github.com>
2024-01-31 01:48:05 -0600

    Update head_start.sh

--- head_start.sh
+++ head_start.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
 # START HEAD NODE
-n=$(sudo -Hiu ubuntu sinfo -o '%C' | sed '2q;d' | sed 's:.*/::')
-cd /home/ubuntu
-echo "$n" > file.txt
-# sudo -Hiu ubuntu setsid nohup ipcluster start --n=$n --profile=slurm > /dev/null 2>&1 & disown
+n=`sudo -Hiu ubuntu sinfo -o '%C' | sed '2q;d' | sed 's:.*/::'`
+sudo -Hiu ubuntu setsid nohup ipcluster start --n=$n --profile=slurm > /dev/null 2>&1 & disown | at now
