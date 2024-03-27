c7251e379445db0580f35a003f08ea930f07fc5b
Anuj Patel <91538343+anuj-p@users.noreply.github.com>
2024-03-27 15:55:27 -0500

    Update slave_init.sh

--- slave_init.sh
+++ slave_init.sh
@@ -5,9 +5,10 @@ apt install python3-mpi4py -y
 
 pip3 install ipyparallel
 
-pip3 install numpy
+pip3 install numpy==1.21
 pip3 install networkx
 pip3 install shapely
 pip3 install matplotlib
 pip3 install geopandas
 pip3 install osmnx
+pip3 install scikit-learn
