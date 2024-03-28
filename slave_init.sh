9255f7612da345c526245e7185f41b882957cedb
Anuj Patel <91538343+anuj-p@users.noreply.github.com>
2024-03-28 03:05:23 -0500

    Update slave_init.sh

--- slave_init.sh
+++ slave_init.sh
@@ -4,17 +4,15 @@
 apt install python3-mpi4py -y
 
 pip3 install ipyparallel
-pip3 install pypi-timemachine
-sudo setsid nohup pypi-timemachine --port 5001 2022-07-18 > /dev/null 2>&1 & disown
 
-pip3 install --index-url http://127.0.0.1:5001/ numpy
-pip3 install --index-url http://127.0.0.1:5001/ fiona
-pip3 install --index-url http://127.0.0.1:5001/ shapely
-pip3 install --index-url http://127.0.0.1:5001/ rtree
-pip3 install --index-url http://127.0.0.1:5001/ pyogrio
-pip3 install --index-url http://127.0.0.1:5001/ pyproj
-pip3 install --index-url http://127.0.0.1:5001/ networkx
-pip3 install --index-url http://127.0.0.1:5001/ matplotlib
-pip3 install --index-url http://127.0.0.1:5001/ geopandas
-pip3 install --index-url http://127.0.0.1:5001/ osmnx
-pip3 install --index-url http://127.0.0.1:5001/ scikit-learn
+pip3 install numpy
+pip3 install fiona
+pip3 install shapely
+pip3 install rtree
+pip3 install pyogrio
+pip3 install pyproj
+pip3 install networkx
+pip3 install matplotlib
+pip3 install geopandas
+pip3 install osmnx
+pip3 install scikit-learn
