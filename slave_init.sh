1a476f4db88f1b5c5794864c7c45446aecb4d1dc
Anuj Patel <91538343+anuj-p@users.noreply.github.com>
2024-04-25 16:57:16 -0500

    Update slave_init.sh

--- slave_init.sh
+++ slave_init.sh
@@ -5,16 +5,4 @@ apt install python3-mpi4py -y
 
 pip3 install ipyparallel
 
-pip3 install numpy
-pip3 install fiona
-pip3 install shapely
-pip3 install rtree
-pip3 install pyogrio
-pip3 install pyproj
-pip3 install networkx
-pip3 install matplotlib
-pip3 install geopandas
-pip3 install osmnx
-pip3 install scikit-learn
-pip3 install imageio
-pip3 install ipywidgets
+pip3 install numpy fiona shapely rtree pyogrio pyproj networkx matplotlib geopandas osmnx scikit-learn imageio ipywidgets
